87213aef50a12310b4d0b703b23497dca0e41768
afzalansari028 <afjalansari13998@gmail.com>
2025-12-30 19:18:32 +0530

    added new function

--- multiply.go
+++ multiply.go
@@ -8,9 +8,15 @@ func main() {
 	b := 20
 	fmt.Println("This sum of two number", a*b)
 
+	Sample()
 	go GoRoutine()
 }
 
-func GoRoutine(){
+func GoRoutine() {
 	fmt.Println("I am goroutine...!!!")
+
+}
+
+func Sample() {
+	fmt.Println("I am go function")
 }
